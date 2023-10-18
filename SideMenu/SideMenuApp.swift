b703c6ea230a3b1e8ab5a16cb29192a155d20b6f
Mohsin Ali Ayub <meismosin@gmail.com>
2023-10-18 19:19:15 +0200

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Mohsin Ali Ayub on 18.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
